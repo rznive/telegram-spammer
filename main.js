0e981f7f4cca65fefa85a15f6c5c1cc283cb8cff
mrzptra <rezaofc.jr@gmail.com>
2024-02-25 14:31:20 +0700

    Update duplicate stop message in main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ async function sendMessage(botToken, chatId, message) {
   const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
   const stopMessage = "\n\n -> if you want to stop this spam, contact https://t.me/riizeisme <-";
-  const fullMessage = message + stopMessage;
+  const fullMessage = message;
 
   const options = {
     method: 'POST',
@@ -18,6 +18,7 @@ async function sendMessage(botToken, chatId, message) {
     json: {
       chat_id: chatId,
       text: fullMessage,
+      parse: "HTML"
     },
   };
 
